337f041e075f3a12a4d926e3cc216ba2e47b3c7d
张钊 <zhangzhaosx2020@163.com>
2022-07-11 21:14:25 +0800

    First commit

---
+++ re模块.py
@@ -0,0 +1,16 @@
+import re
+
+# # findall
+# lst = re.findall(r"\d+", "我的电话号是:17335312104,他的电话是：10010")
+# print(lst)
+#
+# # finditer
+# it = re.finditer(r"\d+", "我的电话号是:17335312104,他的电话是：10010")
+# for i in it:
+#     print(i.group())
+
+# # search
+# s = re.search(r"\d+", "我的电话号是:17335312104,他的电话是：10010")
+# print(s.group())
+
+# match
